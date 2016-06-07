e767eb369acd1dd7b2e0cc83956e08cd89152ac0
alicana <albayrakalican@gmail.com>
2016-06-07 17:44:20 +0300

    recursive pascal triangle calculation

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -3,7 +3,7 @@ package recfun
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
-    for (row <- 0 to 10) {
+    for (row <- 0 to 100) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
@@ -11,17 +11,20 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == r || c == 0) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
+
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
