4a0879a6adfd2ca5e9f199cb9371da7658cb4866
alicana <albayrakalican@gmail.com>
2016-06-09 20:01:00 +0300

    Paranthesis balance implementation

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -3,7 +3,7 @@ package recfun
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
-    for (row <- 0 to 100) {
+    for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
@@ -20,8 +20,36 @@ object Main {
 
   /**
     * Exercise 2
+    *
+    * chars.isEmpty: Boolean returns whether a list is empty
+    * chars.head: Char returns the first element of the list
+    * chars.tail: List[Char] returns the list without the first element
     */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+
+    def balanceCounter(leftParenthesisAcc: Int, rightParenthesisAcc: Int, subChars: List[Char]): Int = {
+      if (subChars.isEmpty || rightParenthesisAcc > leftParenthesisAcc) {
+        leftParenthesisAcc - rightParenthesisAcc
+      } else {
+        val headChar = subChars.head
+
+        if (headChar.equals('(')) {
+          balanceCounter(leftParenthesisAcc + 1, rightParenthesisAcc, subChars.tail)
+        } else if (headChar.equals(')')) {
+          balanceCounter(leftParenthesisAcc, rightParenthesisAcc + 1, subChars.tail)
+        } else {
+          balanceCounter(leftParenthesisAcc, rightParenthesisAcc, subChars.tail)
+        }
+      }
+    }
+
+    if (balanceCounter(0, 0, chars) == 0) {
+     return true
+    }
+
+    false
+
+  }
 
   /**
     * Exercise 3
