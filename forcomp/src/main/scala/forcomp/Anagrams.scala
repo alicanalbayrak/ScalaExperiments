6f88973bf2b9c6844475684f64de720d47521d1a
Ali Can Albayrak <albayrakalican@gmail.com>
2016-07-17 12:56:58 +0300

    combinations impl

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -105,7 +105,13 @@ object Anagrams {
     * Note that the order of the occurrence list subsets does not matter -- the subsets
     * in the example above could have been displayed in some other order.
     */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] = occurrences match {
+    case List() => List(Nil)
+    case (charPart, intPart) :: xs =>
+      val tail = combinations(xs)
+      tail ::: (for (j <- tail; i <- intPart until 0 by -1) yield (charPart, i) :: j)
+  }
+
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
     *
